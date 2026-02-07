f283507e742ca1cd43d16a75f6a251c075babb41
Nelson Urrutia <nelurrutia15@gmail.com>
2026-02-07 12:34:57 -0600

    Feature | improve file search
    Refactor code

--- src/cherry_files_picker/files_utils.py
+++ src/cherry_files_picker/files_utils.py
@@ -26,51 +26,34 @@ def continue_file_path_registration():
 
     return True
 
-
-def get_files_content():
-    """Retrieves a dictionary with files and content"""
-    files_and_content = {}
-    for f in get_cherry_picker_data_files():
-        with open(f) as file:
-            content = file.read()
-            files_and_content[f] = content
-    return files_and_content
-
-def get_file_path():
-    """Retrieves file"""
-    current_working_directory = "./"
+def search_files(file_name:str):
     matching_files = []
-
-    file_name = get_user_input("Enter part or all of the file name", f"\n{BOLD}Search for a file:{ENDBOLD} ", True)
-    
-    selected_files = get_cherry_picker_data_files()
-    if len(selected_files) > 0:
-        print(f"\n {BOLD}Selected files:{ENDBOLD}")
-        for selected in selected_files:
-            print(f" > {selected}")
-    
-
+    current_working_directory = "./"
     for root, _, files in os.walk(current_working_directory):
         for fname in files:
             if file_name in fname:
-                file_obj = {
+                data = {
                     "src": os.path.join(root, fname),
                     "name": fname
                 }
-                matching_files.append(file_obj)
-                
-    if not len(matching_files):
-        print(f" > No files found matching {BOLD}{file_name}{ENDBOLD}.")
-        print("    You can try a shorter or more general name.")
-        get_file_path()
-        
-    
+                matching_files.append(data)
+    return matching_files
+
+def print_matching_files(matching_files: list):
     print(f"\n{BOLD}Matching files found:{ENDBOLD}\n")
     print(f" {BOLD}INDEX{ENDBOLD} {BOLD}FILE NAME{ENDBOLD}\n")
     for i, f in enumerate(matching_files):
         print(f"  [{i}]   {f['name']}")
         print(f"          > Path: {f['src']}\n")
-    
+
+def print_selected_files():
+    selected_files = get_cherry_picker_data_files()
+    if len(selected_files) > 0:
+        print(f"\n {BOLD}Selected files:{ENDBOLD}")
+        for selected in selected_files:
+            print(f" > {selected}")
+
+def register_files(matching_files):
     raw_indexes = get_user_input("File index", f"\nSelect file indexes (comma-separated for multiple): ", True)        
 
     indexes = []
@@ -92,27 +75,34 @@ def get_file_path():
     if len(invalid_indexes) > 0:
         print(f"Invalid index(es): {", ".join(invalid_indexes)}")
 
-    if continue_file_path_registration():
+def get_files_content():
+    """Retrieves a dictionary with files and content"""
+    files_and_content = {}
+    for f in get_cherry_picker_data_files():
+        with open(f) as file:
+            content = file.read()
+            files_and_content[f] = content
+    return files_and_content
+
+
+def get_file_path():
+    """Retrieves file"""
+    file_name = get_user_input("Enter part or all of the file name", f"\n{BOLD}Search for a file:{ENDBOLD} ", True)
+    matching_files = search_files(file_name)
+                
+    if not len(matching_files):
+        print(f" > No files found matching {BOLD}{file_name}{ENDBOLD}.")
+        print("    You can try a shorter or more general name.")
         return get_file_path()
     
-    print(get_cherry_picker_data("files"))
+    print_matching_files(matching_files)
 
-    
-    # file_name = get_user_input("File name", f"\n{BOLD}File name:{ENDBOLD} ", True)
-    # valid_file_path = validate_file_path(file_path)
-    # if valid_file_path:
-    #     set_cherry_picker_data_file(file_path)
-    #     print(f" > File path {BOLD}{file_path}{ENDBOLD} registered")
-    #     continue_registration = continue_file_path_registration()
-    #     if continue_registration:
-    #         get_file_path()
-    # else:
-    #     print(f" > File path {BOLD}{file_path}{ENDBOLD} doesn't exists")
-    #     print(f"   Did not register {BOLD}{file_path}{ENDBOLD}")
-    #     continue_registration = continue_file_path_registration()
-    #     if continue_registration:
-    #         get_file_path()
+    print_selected_files()
 
+    register_files(matching_files)
+
+    if continue_file_path_registration():
+        return get_file_path()
 
 def init_files_utils():
     """Initialize Files utils module"""

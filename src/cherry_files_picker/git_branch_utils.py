d352f45ea5f619ec6ba6619fe982b3d3f848dac9
Nelson Urrutia <nelurrutia15@gmail.com>
2026-02-08 18:18:13 -0600

    Feature | Refactor git branch selection

--- src/cherry_files_picker/git_branch_utils.py
+++ src/cherry_files_picker/git_branch_utils.py
@@ -3,84 +3,57 @@ import subprocess
 from .cherry_picker_data import set_cherry_picker_data
 from .helpers import BOLD, ENDBOLD, get_user_input
 
-
-
 def print_available_branches(branches):
     print(f" {BOLD}INDEX{ENDBOLD}  {BOLD}BRANCH{ENDBOLD}")
     for i, branch in enumerate(branches):
-        branch_name = branch
-        if "*" in branch_name:
-            print(f" [{i}]     {BOLD}{branch_name}{ENDBOLD}")
-        else:
-            print(f" [{i}]     {branch_name}")
+        print(f" [{i}]     {branch}")
         
-
-
-def show_available_branches():
+def get_available_branches():
     result = subprocess.run(["git", "branch", "--list"], capture_output=True, text=True)
-    branches = [line.strip() for line in result.stdout.splitlines()]
-    print_available_branches(branches)
+    branches = [line.replace("*", "").strip() for line in result.stdout.splitlines()]
+    return  branches
     
-
-
 def validate_branch(branch):
     """Validate git branch"""
     result = subprocess.run(
         ["git", "rev-parse", "--verify", branch], capture_output=True, text=True
     )
-
     return result.returncode == 0
 
+def get_branch(branch, handle, branches):
+    branch_index = get_user_input(f"{branch} index", f"\nSelect the {BOLD}{branch} branch.{ENDBOLD} Use the index: ", True)        
 
-def get_source_branch():
-    """Retrieves source branch"""
-    source_branch = get_user_input(
-        "Source branch", f"\n{BOLD}Source branch:{ENDBOLD} ", True
-    )
-    valid_branch = validate_branch(source_branch)
+    if not branch_index.isdigit():
+        print(f"Please try again with a valid index")
+        return get_branch(branch, handle, branches)
+
+    branch_index = int(branch_index)
+
+    if branch_index < 0 or branch_index > len(branches):
+        print(f"Please try again with a valid index")
+        return get_branch(branch, handle, branches)
+    
+    branch = branches[branch_index]
+
+    valid_branch = validate_branch(branch)
+
     if not valid_branch:
-        print(f" > Source branch {BOLD}{source_branch}{ENDBOLD} doesn't exist")
+        print(f" > Branch {BOLD}{branch}{ENDBOLD} is not valid")
         print("   Try again")
-        get_source_branch()
-        return
-
-    print(f" > Source branch {BOLD}{source_branch}{ENDBOLD} is valid")
-    set_cherry_picker_data("source_branch", source_branch)
-
-
-def get_target_branch():
-    """Retrieves target branch"""
-    target_branch = get_user_input(
-        "Target branch", f"\n{BOLD}Target branch:{ENDBOLD} ", True
-    )
-    valid_branch = validate_branch(target_branch)
-    if not valid_branch:
-        print(f" > Target branch {BOLD}{target_branch}{ENDBOLD} doesn't exist")
-        print("   Try again")
-        get_source_branch()
-        return
-
-    print(f" > Target branch {BOLD}{target_branch}{ENDBOLD} is valid")
-    set_cherry_picker_data("target_branch", target_branch)
-
+        return get_branch(branch, handle, branches)
+    
+    print(f" > {BOLD}{branch}{ENDBOLD} selected")
+    set_cherry_picker_data(handle, branch)
 
 def init_branch_utils():
     """Initialize Git Branch module"""
     print(f"\n-- {BOLD}1. BRANCH SETTINGS{BOLD} --\n")
     
-    show_available_branches()
+    branches = get_available_branches()
     
-    source_branch_index = get_user_input("Source branch index", f"\nSelect {BOLD}source branch.{ENDBOLD} Use the index: ", True)        
-    # validate index
-    # repeat if not valid
-    # show selected branch
-    # continue with target branch
-    target_branch_index = get_user_input("Target branch index", f"\nSelect {BOLD}target branch.{ENDBOLD} Use the index: ", True)        
-    # validate index 
-    # repeat if not valid
-    # show selected branch
-    # continue process
+    print_available_branches(branches)
 
+    get_branch("Source","source_branch", branches)
 
-    # get_source_branch()
-    # get_target_branch()
+    get_branch("Target","target_branch", branches)
+

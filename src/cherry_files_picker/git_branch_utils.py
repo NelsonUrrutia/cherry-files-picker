1975d5b29594523af7dbbf33537462525dbe4918
Nelson Urrutia <nelurrutia15@gmail.com>
2026-02-08 18:39:54 -0600

    Feature | Refactor branch selection
    Refactor branch selection process
    
    Source branch: refactor-git-branch-selector

--- src/cherry_files_picker/git_branch_utils.py
+++ src/cherry_files_picker/git_branch_utils.py
@@ -4,49 +4,54 @@ from .cherry_picker_data import set_cherry_picker_data
 from .helpers import BOLD, ENDBOLD, get_user_input
 
 
+def get_available_branches():
+    """Retrieve a list of available branches from Git."""
+    result = subprocess.run(["git", "branch", "--list"], capture_output=True, text=True)
+    branches = [line.replace("*", "").strip() for line in result.stdout.splitlines()]
+    return  branches
+
+def display_available_branches():
+    branches = get_available_branches()
+    """Display a list of available branches."""
+    print(f" {BOLD}INDEX{ENDBOLD}  {BOLD}BRANCH{ENDBOLD}")
+    for i, branch in enumerate(branches):
+        print(f" [{i}]     {branch}")
+        
+
 def validate_branch(branch):
-    """Validate git branch"""
+    """Check if a branch exists in Git."""
     result = subprocess.run(
         ["git", "rev-parse", "--verify", branch], capture_output=True, text=True
     )
-
     return result.returncode == 0
 
+def select_branch(branch, handle):
+    """Prompt the user to select a branch."""
+    branches = get_available_branches()
 
-def get_source_branch():
-    """Retrieves source branch"""
-    source_branch = get_user_input(
-        "Source branch", f"\n{BOLD}Source branch:{ENDBOLD} ", True
-    )
-    valid_branch = validate_branch(source_branch)
-    if not valid_branch:
-        print(f" > Source branch {BOLD}{source_branch}{ENDBOLD} doesn't exist")
-        print("   Try again")
-        get_source_branch()
-        return
-
-    print(f" > Source branch {BOLD}{source_branch}{ENDBOLD} is valid")
-    set_cherry_picker_data("source_branch", source_branch)
-
-
-def get_target_branch():
-    """Retrieves target branch"""
-    target_branch = get_user_input(
-        "Target branch", f"\n{BOLD}Target branch:{ENDBOLD} ", True
-    )
-    valid_branch = validate_branch(target_branch)
-    if not valid_branch:
-        print(f" > Target branch {BOLD}{target_branch}{ENDBOLD} doesn't exist")
-        print("   Try again")
-        get_source_branch()
-        return
-
-    print(f" > Target branch {BOLD}{target_branch}{ENDBOLD} is valid")
-    set_cherry_picker_data("target_branch", target_branch)
-
+    while True:
+        try:
+            branch_index = int(get_user_input(f"{branch} index", f"\nSelect the {BOLD}{branch} branch.{ENDBOLD} Use the index: ", True))        
+            if 0 <= branch_index < len(branches):
+                branch = branches[branch_index]
+                if validate_branch(branch):
+                    print(f" > {BOLD}{branch}{ENDBOLD} selected")
+                    set_cherry_picker_data(handle, branch)
+                    return
+                else:
+                    print(" > Branch is not valid. Please try again.")
+            else:
+                print("Invalid index. Please try again.")
+        except ValueError:
+            print("Please select a number") 
 
 def init_branch_utils():
     """Initialize Git Branch module"""
     print(f"\n-- {BOLD}1. BRANCH SETTINGS{BOLD} --\n")
-    get_source_branch()
-    get_target_branch()
+    
+    display_available_branches()
+
+    select_branch("Source","source_branch")
+
+    select_branch("Target","target_branch")
+

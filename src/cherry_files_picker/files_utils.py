bf8cd72150e4a0e4856105650ee6afb95e78833d
Nelson Urrutia <nelurrutia15@gmail.com>
2026-02-26 20:26:14 -0600

    Refactor | Implement questionary

--- src/cherry_files_picker/files_utils.py
+++ src/cherry_files_picker/files_utils.py
@@ -1,8 +1,8 @@
 import os
+
 from .cherry_picker_data import (
     get_cherry_picker_data_files,
     set_cherry_picker_data_file,
-    get_cherry_picker_data
 )
 from .helpers import BOLD, ENDBOLD, get_user_input
 
@@ -26,18 +26,17 @@ def continue_file_path_registration():
 
     return True
 
-def search_files(file_name:str):
+
+def search_files(file_name: str):
     matching_files = []
     current_working_directory = "./"
     for root, _, files in os.walk(current_working_directory):
         for fname in files:
             if file_name in fname:
-                data = {
-                    "src": os.path.join(root, fname),
-                    "name": fname
-                }
+                data = {"src": os.path.join(root, fname), "name": fname}
                 matching_files.append(data)
     return matching_files
+
 
 def print_matching_files(matching_files: list):
     print(f"\n{BOLD}Matching files found:{ENDBOLD}\n")
@@ -46,6 +45,7 @@ def print_matching_files(matching_files: list):
         print(f"  [{i}]   {f['name']}")
         print(f"          > Path: {f['src']}\n")
 
+
 def print_selected_files():
     selected_files = get_cherry_picker_data_files()
     if len(selected_files) > 0:
@@ -53,19 +53,22 @@ def print_selected_files():
         for selected in selected_files:
             print(f" > {selected}")
 
+
 def register_files(matching_files):
-    raw_indexes = get_user_input("File index", f"\nSelect file indexes (comma-separated for multiple): ", True)        
+    raw_indexes = get_user_input(
+        "File index", f"\nSelect file indexes (comma-separated for multiple): ", True
+    )
 
     indexes = []
     invalid_indexes = []
 
-    for part in  raw_indexes.split(","):
+    for part in raw_indexes.split(","):
         part = part.strip()
         if part.isdigit():
             indexes.append(int(part))
 
     for index in indexes:
-        if  index < len(matching_files):
+        if index < len(matching_files):
             selected = matching_files[index]
             print(f" > Added: {selected['name']}")
             set_cherry_picker_data_file(selected["src"])
@@ -73,7 +76,8 @@ def register_files(matching_files):
             invalid_indexes.append(f"{index}")
 
     if len(invalid_indexes) > 0:
-        print(f"Invalid index(es): {", ".join(invalid_indexes)}")
+        print(f"Invalid index(es): {', '.join(invalid_indexes)}")
+
 
 def get_files_content():
     """Retrieves a dictionary with files and content"""
@@ -87,14 +91,18 @@ def get_files_content():
 
 def get_file_path():
     """Retrieves file"""
-    file_name = get_user_input("Enter part or all of the file name", f"\n{BOLD}Search for a file:{ENDBOLD} ", True)
+    file_name = get_user_input(
+        "Enter part or all of the file name",
+        f"\n{BOLD}Search for a file:{ENDBOLD} ",
+        True,
+    )
     matching_files = search_files(file_name)
-                
+
     if not len(matching_files):
         print(f" > No files found matching {BOLD}{file_name}{ENDBOLD}.")
         print("    You can try a shorter or more general name.")
         return get_file_path()
-    
+
     print_matching_files(matching_files)
 
     print_selected_files()
@@ -103,6 +111,7 @@ def get_file_path():
 
     if continue_file_path_registration():
         return get_file_path()
+
 
 def init_files_utils():
     """Initialize Files utils module"""
--- src/cherry_files_picker/git_branch_utils.py
+++ src/cherry_files_picker/git_branch_utils.py
@@ -1,4 +1,5 @@
 import subprocess
+import questionary
 
 from .cherry_picker_data import set_cherry_picker_data
 from .helpers import BOLD, ENDBOLD, get_user_input
@@ -28,28 +29,14 @@ def validate_branch(branch):
 def select_branch(branch, handle):
     """Prompt the user to select a branch."""
     branches = get_available_branches()
-
-    while True:
-        try:
-            branch_index = int(get_user_input(f"{branch} index", f"\nSelect the {BOLD}{branch} branch.{ENDBOLD} Use the index: ", True))        
-            if 0 <= branch_index < len(branches):
-                branch = branches[branch_index]
-                if validate_branch(branch):
-                    print(f" > {BOLD}{branch}{ENDBOLD} selected")
-                    set_cherry_picker_data(handle, branch)
-                    return
-                else:
-                    print(" > Branch is not valid. Please try again.")
-            else:
-                print("Invalid index. Please try again.")
-        except ValueError:
-            print("Please select a number") 
+    branch = (questionary.select(f"Select the {branch} branch.", choices=branches ).ask())
+    set_cherry_picker_data(handle, branch)
 
 def init_branch_utils():
     """Initialize Git Branch module"""
     print(f"\n-- {BOLD}1. BRANCH SETTINGS{BOLD} --\n")
     
-    display_available_branches()
+    # display_available_branches()
 
     select_branch("Source","source_branch")
 

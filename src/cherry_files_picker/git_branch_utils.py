efc0011e4b302014711e7819c5d1d84028d3e5b7
Nelson Urrutia <nelurrutia15@gmail.com>
2026-02-28 10:44:58 -0600

    Refactor | Files & Branch
    Refactor selection process for files and branches

--- src/cherry_files_picker/git_branch_utils.py
+++ src/cherry_files_picker/git_branch_utils.py
@@ -2,30 +2,14 @@ import subprocess
 import questionary
 
 from .cherry_picker_data import set_cherry_picker_data
-from .helpers import BOLD, ENDBOLD, get_user_input
-
+from .helpers import BOLD, ENDBOLD
 
 def get_available_branches():
     """Retrieve a list of available branches from Git."""
     result = subprocess.run(["git", "branch", "--list"], capture_output=True, text=True)
     branches = [line.replace("*", "").strip() for line in result.stdout.splitlines()]
     return  branches
-
-def display_available_branches():
-    branches = get_available_branches()
-    """Display a list of available branches."""
-    print(f" {BOLD}INDEX{ENDBOLD}  {BOLD}BRANCH{ENDBOLD}")
-    for i, branch in enumerate(branches):
-        print(f" [{i}]     {branch}")
         
-
-def validate_branch(branch):
-    """Check if a branch exists in Git."""
-    result = subprocess.run(
-        ["git", "rev-parse", "--verify", branch], capture_output=True, text=True
-    )
-    return result.returncode == 0
-
 def select_branch(branch, handle):
     """Prompt the user to select a branch."""
     branches = get_available_branches()
@@ -34,11 +18,8 @@ def select_branch(branch, handle):
 
 def init_branch_utils():
     """Initialize Git Branch module"""
-    print(f"\n-- {BOLD}1. BRANCH SETTINGS{BOLD} --\n")
-    
-    # display_available_branches()
-
+    print(f"\n-- {BOLD}1. BRANCH SETTINGS{ENDBOLD} --\n")
     select_branch("Source","source_branch")
-
+    print("\n")
     select_branch("Target","target_branch")
 
